6f39d41ac20e9f3ba9e592b5864669f7f74f5533
Ray Raspberry <ray@protomail.com>
2023-10-09 01:49:49 -0600

    fixed doc edit link + author update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,20 +39,17 @@ const config = {
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+      '@docusaurus/preset-classic',
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/Turkey-DAO/investigations/blob/main/',
+          editUrl: ({versionDocsDirPath, docPath}) =>
+            `https://github.com/Turkey-DAO/investigations/edit/main/${versionDocsDirPath}/${docPath}`,
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Turkey-DAO/investigations/blob/main/',
+          editUrl: ({blogPath}) =>
+            `https://github.com/Turkey-DAO/investigations/edit/main/${blogPath}`,
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -60,7 +57,7 @@ const config = {
       }),
     ],
   ],
-
+  
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
